5e09843769ae41b8fabaaa359da1df6b0bb5dd6d
Siarhei Harelik <kelinso@mail.ru>
2022-02-06 19:00:47 +0300

    Домашка 20

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,20 +1,35 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from blog.views import register
-from home.views import home_index
-from posts.views import post_list, post_add
+from posts.views import post_list, post_add, post_view, posts_user
 from profiles.views import profiles_index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', post_list, name="home"),
-    path('posts/add/', post_add, name="post_add"),
-    path('profiles/', profiles_index, name="profiles_index"),
-    path('register/', register, name="register"),
+    path("admin/", admin.site.urls),
+    path("", post_list, name="home"),
+    path("post/<str:slug>/", post_view, name="post_view"),
+    path("posts/add/", post_add, name="post_add"),
+    path("profiles/", profiles_index, name="profiles_index"),
+    path("register/", register, name="register"),
+    path("posts/posts_user/<str:slug>/", posts_user, name="posts_user"),
     path("api/", include("api.urls", namespace="api")),
-    path('home/', home_index)
 ]
 
 
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,8 +1,10 @@
 import logging
 
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+import profiles
 from posts.forms import PostForm
 from posts.models import Post
 
@@ -12,7 +14,7 @@ logger = logging.getLogger(__name__)
 def post_list(request):
     if request.user.is_anonymous:
         return redirect("admin:index")
-    posts = Post.objects.filter(author=request.user).order_by("-id")
+    posts = Post.objects.order_by("-id")
     return render(request, "posts/list.html", {"posts": posts})
 
 
@@ -27,3 +29,15 @@ def post_add(request):
             form = PostForm()
         return render(request, "posts/add.html", {"form": form})
     return HttpResponse("You don't authenticated!")
+
+
+def post_view(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "posts/view.html", {"post": post})
+
+
+def posts_user(request, slug):
+    logger.info(request)
+    user = str(request)[str(request).rindex('user/')+5:-3]
+    posts = Post.objects.filter(author=str(user))
+    return render(request, "posts/posts_user.html", {"posts": posts})
